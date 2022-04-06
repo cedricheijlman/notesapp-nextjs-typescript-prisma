fd77281b9ca724a3127cde3a062140271e216ca4
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-04-06 12:14:08 +0200

    note update route

--- pages/api/note/[id].ts
+++ pages/api/note/[id].ts
@@ -5,7 +5,22 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const { title, content } = req.body;
   const noteId = req.query.id;
+
+  if (req.method === "POST") {
+    const updatedNote = await prisma.note.update({
+      where: {
+        id: Number(noteId),
+      },
+      data: {
+        title,
+        content,
+      },
+    });
+
+    return res.status(200).json({ message: "succesfully updated note" });
+  }
 
   if (req.method === "DELETE") {
     const note = await prisma.note.delete({
@@ -13,6 +28,6 @@ export default async function handler(
     });
     return res.json(noteId);
   } else {
-    return res.json({ message: "couldn't delete note" });
+    return res.json({ message: "error" });
   }
 }
